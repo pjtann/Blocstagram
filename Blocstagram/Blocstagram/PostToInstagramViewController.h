b0a7f1f6f7a872ca45ce8b1e5ef296216405cca3
PT <PT@PTs-MacBook-Pro.local>
2016-04-01 13:58:34 -0600

    Checkpoint Foundation 43 Filter and Submit Images Completed

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,17 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by PT on 4/1/16.
+//  Copyright (c) 2016 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+
+-(instancetype) initWithImage:(UIImage *) sourceImage;
+
+
+@end
